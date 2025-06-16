8dd641ea08ff293875c8219363087bf126c1a9cf
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-06-16 15:09:05 -0400

    get card images to display onClick

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import "./Homepage.css";
 import logo from "../../assets/jakecasinologo.png";
+import play from "../../assets/jakecasinoplay.png";
 
 function Homepage() {
   return (
     <section className="homepage">
       <img className="homepage__logo" src={logo} alt="logo" />
       <Link to="/game">
-        <button className="homepage__btn">Play!</button>
+        <button className="homepage__btn">
+          <img src={play} alt="play" />
+        </button>
       </Link>
     </section>
   );
